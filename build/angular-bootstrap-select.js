dbb91e10218a9ead91a7222841cf303c900e9fa8
Rowland Watkins <rowland.watkins@gmail.com>
2014-10-04 19:05:13 +0800

    naming fix

--- build/angular-bootstrap-select.js
+++ build/angular-bootstrap-select.js
@@ -35,21 +35,21 @@ angular.module('angular-bootstrap-select', [])
       require: '?ngModel',
       priority: 10,
       compile: function (tElement, tAttrs, transclude) {
-        tElement.bsselectpicker($parse(tAttrs.bsselectpicker)());
-        tElement.bsselectpicker('refresh');
+        tElement.selectpicker($parse(tAttrs.bsselectpicker)());
+        tElement.selectpicker('refresh');
         return function (scope, element, attrs, ngModel) {
           if (!ngModel) return;
 
           scope.$watch(attrs.ngModel, function (newVal, oldVal) {
             scope.$evalAsync(function () {
               if (!attrs.ngOptions || /track by/.test(attrs.ngOptions)) element.val(newVal);
-              element.bsselectpicker('refresh');
+              element.selectpicker('refresh');
             });
           });
 
           ngModel.$render = function () {
             scope.$evalAsync(function () {
-              element.bsselectpicker('refresh');
+              element.selectpicker('refresh');
             });
           }
         };
